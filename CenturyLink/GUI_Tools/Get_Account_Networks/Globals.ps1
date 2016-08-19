d830823f99a5047d72622954ad3c550fa55f50ce
davissea <sean.davis@ihg.com>
2016-08-19 12:05:14 -0400

    new file:   CenturyLink/GUI_Tools/Get_Account_Networks/CL Sub Account POC.psproj 	new file:   CenturyLink/GUI_Tools/Get_Account_Networks/CL Sub Account POC.psproj.psbuild 	new file:   CenturyLink/GUI_Tools/Get_Account_Networks/CL Sub Account POC.psprojs 	new file:   CenturyLink/GUI_Tools/Get_Account_Networks/Globals.ps1 	new file:   CenturyLink/GUI_Tools/Get_Account_Networks/MainForm.psf 	new file:   CenturyLink/GUI_Tools/Get_Account_Networks/MainForm.psf.psbuild 	modified:   CenturyLink/GUI_Tools/Get_Account_Networks/README.md 	new file:   CenturyLink/GUI_Tools/Get_Account_Networks/Startup.pss 	new file:   CenturyLink/GUI_Tools/Get_Account_Networks/Startup.pss.psbuild

---
+++ CenturyLink/GUI_Tools/Get_Account_Networks/Globals.ps1
@@ -0,0 +1,38 @@
+﻿#--------------------------------------------
+# Declare Global Variables and Functions here
+#--------------------------------------------
+
+function Get-CL_Accounts
+{
+	#Go Grab All The Users
+	$RequestURL = 'https://api.ctl.io/REST/Account/GetAccounts/JSON'
+	$Content = Invoke-WebRequest -URI $RequestURL -Method POST -ContentType application/json -WebSession $session
+	$Users = $Content.Content
+
+	#Show Me The User Objects
+	($Users | ConvertFrom-JSON).Accounts
+}
+
+function Get-CL_Networks
+{
+	#Go Grab All The Users
+	$RequestURL = 'https://api.ctl.io/REST/Network/GetNetworks/JSON'
+	$Content = Invoke-WebRequest -URI $RequestURL -Method POST -ContentType application/json -WebSession $session
+	$Networks = $Content.Content
+
+	#Show Me The User Objects
+	($Networks | ConvertFrom-JSON).Networks
+}
+
+function Get-CL_ParentCompany
+{
+	param ($Account)
+	
+	#Go Grab All The Users
+	$RequestURL = 'https://api.ctl.io/REST/Account/GetAccounts/JSON'
+	$Content = Invoke-WebRequest -URI $RequestURL -Method POST -ContentType application/json -WebSession $session
+	$Users = $Content.Content
+	
+	#Show Me The User Objects
+	return $(($Users | ConvertFrom-JSON).Accounts | where {$_.AccountAlias -eq $Account }).ParentAlias
+}
--- CenturyLink/GUI_Tools/Get_Account_Networks/README.md
+++ CenturyLink/GUI_Tools/Get_Account_Networks/README.md
@@ -1,1 +1,3 @@
-README
+SAPIEN PowerShell Studio Project
+
+#You Will Need To Modify The Code And Put Your Company API KEy And Pass In Before It Will Work
