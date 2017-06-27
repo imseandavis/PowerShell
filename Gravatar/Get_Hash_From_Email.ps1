0bf4aa6f110b6a7b0989a5cf1b049cc89218929f
Sean Davis <imseandavis@gmail.com>
2017-06-27 11:36:51 -0400

    Update Get_Hash_From_Email.ps1
    finally updated ot finish this one, easier than I thought. May need error handling in case someone'e email isn't legit but I'll do it later.

--- Gravatar/Get_Hash_From_Email.ps1
+++ Gravatar/Get_Hash_From_Email.ps1
@@ -1,9 +1,21 @@
-#Gravatar URL : https://www.gravatar.com/205e460b479e2e5b48aec07710c08d50.json
-
 #Gravatar Icon In Powershell
+#Gravatar URL : https://secure.gravatar.com/avatar/237f9c44d3a4e5ae2274eab736e3231c
+
+#Define Variables
 $Email = "imseandavis@gmail.com"
 
 #Convert The Email To MD5 Hash
 $StringBuilder = New-Object System.Text.StringBuilder
 [System.Security.Cryptography.HashAlgorithm]::Create('MD5').ComputeHash([System.Text.Encoding]::UTF8.GetBytes($Email))| ForEach {[Void]$StringBuilder.Append($_.ToString("x2"))}
 $Hash = $StringBuilder.ToString()
+
+#Get Profile Info
+$ProfileRequest = (Invoke-RestMethod -URI https://www.gravatar.com/$Hash.json).Entry
+
+#Display Profile Info
+Write-Host "Profile ID: $($ProfileRequest.ID)"
+Write-Host "Profile URL: $($ProfileRequest.ProfileURL)"
+Write-Host "Profile UserName: $($ProfileRequest.PreferredUserName)"
+Write-Host "Profile Thumbnail URL: $($ProfileRequest.ThumbnailURL)"
+Write-Host "Profile Name: $($ProfileRequest.Name.Formatted)"
+Write-Host "Profile About Me: $($ProfileRequest.AboutMe)"
